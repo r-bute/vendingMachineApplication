c5c377ef1ecdbd44df4cd8d883947c6dcd287ed5
ronald_b <68206329+r-bute@users.noreply.github.com>
2024-07-25 16:04:21 -0400

    To complete vending machine class

--- src/main/java/com/perscholas/java_basics/VendRunner.java
+++ src/main/java/com/perscholas/java_basics/VendRunner.java
@@ -3,10 +3,20 @@ package com.perscholas.java_basics;
 public class VendRunner {
     public static void main(String[] args) {
         //snach instance
+        Snack mySnack = new Snack();
 
         //beverage instance
+        Beverage myBeverage = new Beverage();
+
 
         //slot instance
+        VendingMachine myVendingMachine = new VendingMachine();
+
+        myVendingMachine.addProduct("mySnack,");
+        //to be completed
+
+
+
 
     }
 }
--- src/main/java/com/perscholas/java_basics/VendingMachine.java
+++ src/main/java/com/perscholas/java_basics/VendingMachine.java
@@ -1,7 +1,62 @@
 package com.perscholas.java_basics;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class VendingMachine {
-    slots (Map<String, Slot<? extends Product>>)
+    //Attribute store the slots i.e String product codes are linked to the product object
+
+
+   private Map<String, Slot<? extends Product>>Slot;//this is a collection interface representing K,V key value pairs
+
+    public VendingMachine() {
+        this.Slot = new HashMap<>();
+    }
+
+    //method to addProduct
+    public <T extends Product> void addProduct(String code, Slot<T> slot) {
+        this.Slot.put(code, slot);
+    }
+
+    //method to dispenseProduct
+    public Product dispenseProduct (String code) {
+
+        Slot slot = this.Slot.get(code);
+        // checking if the slot even exists
+        if (slot != null && slot.getQuantity() > 0 ) {
+            slot.setQuantity(slot.getQuantity() - 1);
+            return slot.getProduct();
+        }
+        else {
+            System.out.println("Product not available");
+            return null;
+        }
+
+
+    }
+
+    //method to displayProducts
+    public  void displayProducts() {
+           /*
+            * The Map's values are Slot objects. The wildcard ? extends Product indicates
+            * that the map can store Slot objects containing any type that extends the
+            * Product class.
+            */
+        for (Map.Entry<String, Slot<? extends Product>> entry : this.Slot.entrySet())
+        {
+            String code = entry.getKey();
+            Slot<? extends Product> slot = entry.getValue();
+
+            // Display the product details
+            System.out.println("Code: " + code + ", Slot: " + slot);;
+        }
+        /*More personal notes
+        *Slot<T> :is the generic type representing all the product types
+        *'?' Wildcard :used the wildcard to enable user  to enter  any type of Product as a subclass of slot.
+        *extends product help constrain the wildcard to values or objects only contained in slot.
+         */
+
+    }
+
+
 }
